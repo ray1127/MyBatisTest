38143e22206ca53302057072b512b1e44150e3e7
xwjiang <jiangxinwei1997@163.com>
2022-02-27 16:31:18 +0800

    开始

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @author: xwjiang
+ * @Version: 1.0
+ * @date 2022/2/27 16:28
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("MyBatis测试");
+    }
+}
